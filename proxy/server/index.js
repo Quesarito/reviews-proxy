4fa761ef83411216274c6f420bf021561bb18493
klarok <karaylu@gmail.com>
2019-01-02 20:07:59 -0800

    Deleted components in order to replace with submodules

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8080;
+const port = 3000;
 
 app.use(bodyParser());
 app.use(express.static('public'));
 
 app.get('/reviews/:productId', (req, res) => {
-  res.redirect('http://localhost:3000' + req.path);
+  res.redirect('http://localhost:3003' + req.path);
   // console.log(req.url);
   // res.send('hello');
 });

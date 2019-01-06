fe9b3dadae611665d07f11f3610977697be92bea
klarok <karaylu@gmail.com>
2019-01-05 21:03:56 -0800

    Fixes proxy routes, adds navbar, adds related items data and styles

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -2,41 +2,71 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
+const axios = require('axios');
 const port = 3000;
-const domain = 'http://localhost:';
 
 app.use(bodyParser());
 app.use(express.static('public'));
 
 app.get('/api/products', (req, res) => {
-  let url = `http://localhost:3002/api/products?id=${req.query.id}`;
-  request(url, (err, response) => {
-    res.send(response.clone());
-  });
+  axios.get(`http://54.83.144.162:3002/api/products?id=${req.query.id}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING PRODUCT', err);
+    });
 });
 
 app.get('/reviews/:productId', (req, res) => {
-  request(`http://ec2-54-153-13-213.us-west-1.compute.amazonaws.com:3003/reviews/${req.params.productId}`, (err, response) => {
-    res.send(response.body);
-  });
+  axios.get(`http://ec2-54-153-13-213.us-west-1.compute.amazonaws.com:3003/reviews/${req.params.productId}${'?reviewType=' + req.query.reviewType}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING REVIEWS', err);
+    });
 });
 
-app.get('/api/related/:productId', (req, res) => {
-  request(`http://localhost:3001/api/related/${req.params.productId}`, (err, response) => {
-    res.send(response.body);
-  });
+app.get('/api/items', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/items`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING ALL RELATED PRODUCTS', err);
+    });
 });
 
-app.get('/api/items/:productId', (req, res) => {
-  request(`http://localhost:3001/api/items/${req.params.productId}`, (err, response) => {
-    res.send(response.body);
-  });
+app.get('/api/items/:itemId', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/items/${req.params.itemId}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING ALL ITEMS', err);
+    });
 });
 
-app.get('/api/frequent/:productId', (req, res) => {
-  request(`http://localhost:3001/api/frequent/${req.params.productId}`, (err, response) => {
-    res.send(response.body);
-  });
+app.get('/api/related/:itemId', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/related/${req.params.itemId}`)
+    .then(response => {
+      console.log(req.url);
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING RELATED PRODUCTS', err);
+    });
+});
+
+app.get('/api/frequent/:itemId', (req, res) => {
+  axios.get(`http://ec2-54-183-132-22.us-west-1.compute.amazonaws.com/api/frequent/${req.params.itemId}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('ERROR GETTING FREQUENT ITEMS', err);
+    });
 });
 
 app.listen(port, err => {
